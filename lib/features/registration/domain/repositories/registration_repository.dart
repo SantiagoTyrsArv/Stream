1da53e106dedc5d5ffc572d6509179c6e323e50a
Santiago Arevalo <santif.arevalo@gmail.com>
2026-04-29 18:11:34 -0500

    add jni ffi plugin configuration to linux and windows generated_plugins files

--- lib/features/registration/domain/repositories/registration_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/registration_form.dart';
-
-/// Contrato abstracto para el repositorio de registro.
-/// La presentación depende de esta abstracción, no de la implementación.
-abstract class RegistrationRepository {
-  Future<bool> register(RegistrationForm form);
-}
